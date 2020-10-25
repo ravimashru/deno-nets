d926d80c141ebc6748ee9e0d9c4289bb7378afd6
Ravi Mashru <me@ravimashru.dev>
2020-10-25 17:05:34 +0530

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,6 +1,2 @@
+export * from './mnist-data-loader.ts'
 export * from './data-loader-test.ts'
-export * from './data-reader-example.ts'
-export * from './mnist-data-loader.ts'
-export * from './mnist-data-loader.ts'
-export * from './webworker-runthis.ts'
-export * from './worker.ts'
