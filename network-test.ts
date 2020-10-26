ff5bd771a33c7cbf084c76dd864a4a1df7f65291
Chandan Kumar <chandan.kumar02@sap.com>
2020-10-26 21:10:48 +0530

    Add MNIST Test Example

--- network-test.ts
+++ network-test.ts
@@ -1,20 +1,38 @@
 import { Matrix } from 'https://deno.land/x/math/mod.ts';
+import { MNISTDataLoader } from './mnist-data-loader.ts';
 import { Network } from './network.ts';
 
-const net = new Network([2, 5, 3]);
 
-const X_train = new Matrix([
-  [0, 0],
-  [0, 1],
-  [1, 0],
-  [1, 1],
-]);
+const runMNISTExample = async () => {
+    const net = new Network([2, 5, 3]);
+    const dataLoader = new MNISTDataLoader();
+    const mnistDataset = await dataLoader.load_train();
+    const X_train = mnistDataset[0]
+    const y_train = mnistDataset[1]
+    net.train(X_train, y_train, 100001, 1);
+}
 
-const y_train = new Matrix([
-  [-1, -1, -1],
-  [-1, 1, 1],
-  [-1, 1, 1],
-  [1, 1, -1],
-]);
+const runLogicGateExample = async () => {
+    const net = new Network([2, 5, 3]);
+    const X_train = new Matrix([
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ]);
+    const y_train = new Matrix([
+      [-1, -1, -1],
+      [-1, 1, 1],
+      [-1, 1, 1],
+      [1, 1, -1],
+    ]);
+    net.train(X_train, y_train, 100001, 1);
+}
 
-net.train(X_train, y_train, 100001, 1);
+// runMNISTExample();
+runLogicGateExample();
+
+
+
+
+
