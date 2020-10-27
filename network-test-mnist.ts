5952b996fc0dad24bddeb9eb67627386e4755a9e
Chandan Kumar <chandan.kumar02@sap.com>
2020-10-27 10:49:29 +0530

    WIP: mnist dataloader - add hot encoding

--- network-test-mnist.ts
+++ network-test-mnist.ts
@@ -1,6 +1,7 @@
 import { GzipStream } from 'https://deno.land/x/compress@v0.3.3/mod.ts';
 import { MNISTDataLoader } from './mnist-data-loader.ts';
 import { Network } from './network.ts';
+import { onehotencoder } from './utility.ts';
 
 const runMNISTExample = async () => {
     const net = new Network([784, 30, 10]);
@@ -8,7 +9,8 @@ const runMNISTExample = async () => {
     const mnistDataset = await dataLoader.load_train();
     const X_train = mnistDataset[0]
     const y_train = mnistDataset[1]
-    net.train(X_train, y_train, 100001, 1);
+    const y_train_encoded = onehotencoder(y_train)
+    net.train(X_train, y_train_encoded, 100001, 1);
 }
 
 runMNISTExample()
